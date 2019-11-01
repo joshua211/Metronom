ea249ab4fdc5f6377bbab922b59083996a0a9a55
Lycosh <Lycosh211@gmx.de>
2019-11-01 11:23:12 +0100

    starting point

---
+++ src/metronom.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Metronom extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  
+        <div>
+            Nothing to render
+        </div>);
+    }
+}
+ 
+export default Metronom;
