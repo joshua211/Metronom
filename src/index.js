b287ff77f376e3998c827f374658b82462b9ceee
Lycosh <Lycosh211@gmx.de>
2019-11-03 15:30:45 +0100

    added styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import Metronom from './metronom';
 import * as serviceWorker from './serviceWorker';
 
